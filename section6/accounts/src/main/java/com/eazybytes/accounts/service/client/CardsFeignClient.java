f58bbae5f27a307c6686fcf5a302dbc9f958dec5
Jagdish <jagdishkumarsutar@gmail.com>
2026-06-24 20:24:22 +0530

    Service registry and descovery and Centralized configuration inside git

---
+++ section6/accounts/src/main/java/com/eazybytes/accounts/service/client/CardsFeignClient.java
@@ -0,0 +1,18 @@
+package com.eazybytes.accounts.service.client;
+
+import com.eazybytes.accounts.dto.CardsDto;
+import com.eazybytes.accounts.dto.LoansDto;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "cards")
+public interface CardsFeignClient {
+
+    @GetMapping(path = "/api/fetch", consumes = "application/json")
+    public ResponseEntity<CardsDto> fetchCardDetails(@RequestParam
+                                                     String mobileNumber);
+
+}
